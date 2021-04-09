fc6ce2ec4a8acd7696e47dec5f458e561a8f4841
Chad Niedert <>
2021-04-09 02:33:04 +0000

    created count for first 10 natural numbers

--- Main.java
+++ Main.java
@@ -1,7 +1,21 @@
 // Link:  https://projecteuler.net/problem=6
 
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+		int sumSquares = 0;
+		int squareOfSum = 0;
+
+		for (int i = 1; i < 11; i++)
+		{
+			sumSquares += i * i;
+		}
+
+		for (int i = 1; i < 11; i++)
+		{
+			squareOfSum += i;
+		}
+		squareOfSum *= squareOfSum;
+
+		System.out.println(squareOfSum - sumSquares);
+    }
 }
